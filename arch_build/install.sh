0aa001a30f201d3f927b8e87ae265a83e769f16f
euler <amr.euler@gmail.com>
2026-02-26 20:22:19 +0200

    New Updates

--- arch_build/install.sh
+++ arch_build/install.sh
@@ -1,29 +1,37 @@
 #!/bin/bash
 
-# Dev
-sudo pacman -S --noconfirm -- needed base-devel git wget curl openssh stow vulkan-radeon zed
-
-# Suckless
-sudo pacman -S --noconfirm --needed libx11 libxft libxinerama xorg-server xorg-xinit sxhkd
-
 # Network
 sudo pacman -S --noconfirm --needed networkmanager 
+
 # Bluetooth
 sudo pacman -S --noconfirm --needed bluez bluez-utils 
+
 # Audio
-sudo pacman -S --noconfirm --needed pipewire pipewire-audio pipewire-pulse pipewire-alsa alsa-utils
+sudo pacman -S --noconfirm --needed pipewire pipewire-audio pipewire-pulse pipewire-alsa alsa-utils Pulsemixer
 
 # Fonts
-sudo pacman -S --noconfirm --needed noto-fonts noto-fonts-emoji ttf-jetbrains-mono
+sudo pacman -S --noconfirm --needed noto-fonts noto-fonts-emoji ttf-jetbrains-mono-nerd
+
+# Suckless
+sudo pacman -S --noconfirm --needed xorg-server xorg-xsetroot xorg-xinit libxinerama libx11 libxft
+
+# Dev
+sudo pacman -S --noconfirm --needed base-devel git wget curl openssh stow doas
 
 # Web
 sudo pacman -S --noconfirm --needed firefox chromium
 
 # Terminal
-sudo pacman -S --noconfirm --needed tmux vim nnn btop htop fzf
+sudo pacman -S --noconfirm --needed kitty tmux nvim nnn btop fzf unzip 
+
+# Text Editor
+sudo pacman -S --noconfirm --needed vulkan-radeon zed 
 
 # Media
-sudo pacman -S --noconfirm --needed sxiv zathura mpv mpd unzip scrot obsidian
+sudo pacman -S --noconfirm --needed sxiv zathura mpv mpd scrot 
+
+# Notes
+sudo pacman -S --noconfirm --needed obsidian
 
 # VM
 sudo pacman -S --noconfirm --needed gnome-boxes 
