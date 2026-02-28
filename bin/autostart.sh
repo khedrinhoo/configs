b63fda0bbf6e76e68591032f7b7258459651c193
euler <amr.euler@gmail.com>
2026-02-28 12:57:47 +0200

    suckless config

--- bin/autostart.sh
+++ bin/autostart.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 setxkbmap -layout us,ara -variant ,digits -option grp:alt_shift_toggle caps:escape &
+xsetroot -solid "#141415" &
 sxhkd &
--- suckless/dwm/config.def.h
+++ suckless/dwm/config.def.h
@@ -13,19 +13,19 @@ static const int topbar             = 0;        /* 0 means bottom bar */
 static const int splitstatus        = 1;        /* 1 for split status items */
 static const char *splitdelim       = ";";       /* Character used for separating status */
 
-static const char *fonts[]          = { "JetBrainsMono Nerd Font Bold:size=12" };
-static const char dmenufont[]       = "JetBrainsMono Nerd Font Bold:size=12";
+static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=12" };
+static const char dmenufont[]       = "JetBrainsMono Nerd Font:size=12";
 
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_gray1[]       = "#141415";
+static const char col_gray2[]       = "#252530";
+static const char col_gray3[]       = "#cdcdcd";
+static const char col_gray4[]       = "#cdcdcd";
+static const char col_cyan[]        = "#252530";
 
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_gray3 },
 };
 
 /* tagging */
